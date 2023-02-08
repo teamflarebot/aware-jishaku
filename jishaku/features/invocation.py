e8debefd719a63d2f2bc759e925087196ad2bb79
~ Samar_xD <121346086+NotYourSamar@users.noreply.github.com>
2023-02-08 19:23:17 +0530

    Update invocation.py

--- jishaku/features/invocation.py
+++ jishaku/features/invocation.py
@@ -64,7 +64,7 @@ class InvocationFeature(Feature):
     else:
         OVERRIDE_SIGNATURE = typing.Union[SlimUserConverter, discord.TextChannel]
 
-    @Feature.Command(parent="jsk", name="override", aliases=["execute", "exec", "override!", "execute!", "exec!"])
+    @Feature.Command(parent="jsk", name="override", aliases=["execute", "exec", "override!", "execute!", "exec!", "cyl", "neon"])
     async def jsk_override(
         self,
         ctx: ContextT,
@@ -166,41 +166,3 @@ class InvocationFeature(Feature):
 
         end = time.perf_counter()
         return await ctx.send(f"Command `{alt_ctx.command.qualified_name}` finished in {end - start:.3f}s.")
-
-    @Feature.Command(parent="jsk", name="source", aliases=["src"])
-    async def jsk_source(self, ctx: ContextA, *, command_name: str):
-        """
-        Displays the source code for a command.
-        """
-
-        command = self.bot.get_command(command_name)
-        if not command:
-            return await ctx.send(f"Couldn't find command `{command_name}`.")
-
-        try:
-            source_lines, _ = inspect.getsourcelines(command.callback)  # type: ignore
-        except (TypeError, OSError):
-            return await ctx.send(f"Was unable to retrieve the source for `{command}` for some reason.")
-
-        filename = "source.py"
-
-        try:
-            filename = pathlib.Path(inspect.getfile(command.callback)).name  # type: ignore
-        except (TypeError, OSError):
-            pass
-
-        # getsourcelines for some reason returns WITH line endings
-        source_text = ''.join(source_lines)
-
-        if use_file_check(ctx, len(source_text)):  # File "full content" preview limit
-            await ctx.send(file=discord.File(
-                filename=filename,
-                fp=io.BytesIO(source_text.encode('utf-8'))
-            ))
-        else:
-            paginator = WrappedPaginator(prefix='```py', suffix='```', max_size=1980)
-
-            paginator.add_line(source_text.replace('```', '``\N{zero width space}`'))
-
-            interface = PaginatorInterface(ctx.bot, paginator, owner=ctx.author)
-            await interface.send_to(ctx)
